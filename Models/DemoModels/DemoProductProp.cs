076eed4514ef0295dbf22fc00b11970f53caa636
Mina Latif <mina.latif@wdx.eg>
2023-10-24 11:39:21 +0300

    One way and Two way binding

---
+++ Models/DemoModels/DemoProductProp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DemoModels
+{
+    public class DemoProductProp
+    {
+        public int ID { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+
+
+    }
+}
