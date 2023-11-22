6a4fcf02089902556ec494d5b790233b02497b98
Mina Latif <mina.latif@wdx.eg>
2023-11-22 13:28:10 +0200

    Using Toastr from JS

---
+++ TangyBlazor_Server/Data/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace TangyBlazor_Server.Data.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToastrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+        public static async ValueTask ToastrFalure(this IJSRuntime jSRuntime , string message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
