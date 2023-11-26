fa319ddf435b2361514263ccb08fc6fe350487be
Mina Latif <mina.latif@wdx.eg>
2023-11-26 11:26:01 +0200

    ICategoryRepository

---
+++ TangyBlazor_UseCases/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TangyBlazor_UseCases.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO category);
+        public CategoryDTO Update(CategoryDTO category);
+        public CategoryDTO Delete(int id);
+        public CategoryDTO Get(int id);
+        public IEnumerable<CategoryDTO> GetAll();
+    }
+}
